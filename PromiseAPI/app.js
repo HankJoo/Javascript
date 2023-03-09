4f4277f7bb72287ba1d28d5d56a461a7ed65bf70
HankJoo <wink586@naver.com>
2023-03-09 23:23:36 +0900

    promise API

--- PromiseAPI/app.js
+++ PromiseAPI/app.js
@@ -75,3 +75,92 @@ if(!Promise.allSettled) {
         }))));
     };
 }
+
+// Promise 클래스에는 5가지 정적 메서드가 있습니다.
+// Promise.all(promises) – 모든 프라미스가 이행될 때까지 기다렸다가 그 결과값을 담은 배열을 반환합니다.
+// 주어진 프라미스 중 하나라도 실패하면 Promise.all는 거부되고, 나머지 프라미스의 결과는 무시됩니다.
+// Promise.allSettled(promises) – 최근에 추가된 메서드로 모든 프라미스가 처리될 때까지 기다렸다가
+// 그 결과(객체)를 담은 배열을 반환합니다.
+// 객체엔 다음과 같은 정보가 담깁니다.   ****status: "fulfilled" 또는 "rejected"******
+// value(프라미스가 성공한 경우) 또는 reason(프라미스가 실패한 경우)
+
+// 응답이 성공할 경우 – {status:"fulfilled", value:result}
+// 에러가 발생한 경우 – {status:"rejected",  reason:error}
+
+// Promise.race(promises) – 가장 먼저 처리된 프라미스의 결과 또는 에러를 담은 프라미스를 반환합니다.
+// Promise.resolve(value) – 주어진 값을 사용해 이행 상태의 프라미스를 만듭니다.
+// Promise.reject(error) – 주어진 에러를 사용해 거부 상태의 프라미스를 만듭니다.
+
+
+function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+
+    script.onload = () => callback(null, script);
+    script.onerror = () => callback(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
+
+    document.head.append(script);
+}
+
+// 사용법:
+//  loadScript('path/script.js', (err, script) => {...})
+
+
+let loadScriptPromise = function(src) {
+    return new Promise((resolve, reject) => {
+        loadScript(src, (err, script) => {
+            if (err) reject(err)
+            else resolve(script);
+        });
+    })
+}
+
+// 사용법:
+// loadScriptPromise('path/script.js').then(...)
+
+function promisify(f) {
+    return function (...args) { // 래퍼 함수를 반환함
+        return new Promise((resolve, reject) => {
+            function callback(err, result) { // f에 사용할 커스텀 콜백
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            }
+
+            args.push(callback); // 위에서 만든 커스텀 콜백을 함수 f의 인수 끝에 추가합니다.
+
+            f.call(this, ...args); // 기존 함수를 호출합니다.
+        });
+    };
+};
+
+// 사용법:
+let loadScriptPromise = promisify(loadScript);
+// loadScriptPromise(...).then(...);
+
+
+// 콜백의 성공 결과를 담은 배열을 얻게 해주는 promisify(f, true)
+function promisify(f, manyArgs = false) {
+    return function (...args) {
+        return new Promise((resolve, reject) => {
+            function callback(err, ...results) { // f에 사용할 커스텀 콜백
+                if (err) {
+                    reject(err);
+                } else {
+                    // manyArgs가 구체적으로 명시되었다면, 콜백의 성공 케이스와 함께 이행 상태가 됩니다.
+                    resolve(manyArgs ? results : results[0]);
+                }
+            }
+
+            args.push(callback);
+
+            f.call(this, ...args);
+        });
+    };
+};
+
+// 사용법:
+f = promisify(f, true);
+// f(...).then(arrayOfResults => ..., err => ...)
